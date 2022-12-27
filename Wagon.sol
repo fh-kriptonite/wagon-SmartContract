069e3c3e0bc5e32bb79074d84c593a1797cef69c
fh-kriptonite <fh@kriptonite.co>
2022-12-27 18:26:35 +0700

    Update Wagon.sol
    Add governance voting compatibility for WAG token

--- Wagon.sol
+++ Wagon.sol
@@ -16,10 +16,12 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract Wagon is ERC20, ERC20Burnable, ERC20Snapshot, AccessControl, Pausable {
+contract Wagon is ERC20, ERC20Burnable, ERC20Snapshot, AccessControl, Pausable, ERC20Permit, ERC20Votes {
     bytes32 public constant SNAPSHOT_ROLE = keccak256("SNAPSHOT_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
@@ -39,7 +41,7 @@ contract Wagon is ERC20, ERC20Burnable, ERC20Snapshot, AccessControl, Pausable {
     // Setting all the roles needed and mint the initial supply.
     // @param "Wagon" Token Name
     // @param "WAG" Token Symbol
-    constructor() ERC20("Wagon", "WAG") {
+    constructor() ERC20("Wagon", "WAG") ERC20Permit("Wagon") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(SNAPSHOT_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
@@ -153,5 +155,28 @@ contract Wagon is ERC20, ERC20Burnable, ERC20Snapshot, AccessControl, Pausable {
         uint dirtyFunds = balanceOf(_blackListedUser);
         _transfer(_blackListedUser, emergencyAccount, dirtyFunds);
         emit RecoveredBlackFunds(_blackListedUser, dirtyFunds);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
     }
 }
